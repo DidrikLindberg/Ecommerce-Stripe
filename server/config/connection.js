5ca90eca796e8f97e09746918f6500d9b4a931fe
GitDidrik <lindberg.didrik@gmail.com>
2023-09-27 17:14:31 -0700

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/SmartProducts';
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/test';
 
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
